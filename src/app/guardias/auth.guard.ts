606f4f25f0c0df282599c5549006cb9864c49c2e
jk98 <davidlrj9999@gmail.com>
2024-02-21 14:52:27 -0600

    redirec guardia login-home

--- src/app/guardias/auth.guard.ts
+++ src/app/guardias/auth.guard.ts
@@ -1,9 +1,12 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
-
+import { Router } from '@angular/router';
 export const authGuard: CanActivateFn = (route, state) => {
   if(sessionStorage.getItem('email')){
     return true;
   }else{
+    let route = inject(Router);
+    route.navigate(['login']);
     return false;
   }
 };
